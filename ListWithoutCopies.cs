e397bd661049100585e174fefe3432f93e385b39
Chris O'Brien <chrisobrien2000@outlook.com>
2020-04-13 10:28:29 -0500

    Added Method that Removes Copies From List

---
+++ ListWithoutCopies.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CopyListWithoutDuplicates
+{
+    class ListWithoutCopies
+    {
+        List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+
+        public List<string> RemoveDuplicates()
+        {
+            var namesWithoutDuplicates = names.Distinct().ToList();
+            return namesWithoutDuplicates;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CopyListWithoutDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListWithoutCopies newList = new ListWithoutCopies();
+            var list = newList.RemoveDuplicates();
+            foreach(string name in list)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
